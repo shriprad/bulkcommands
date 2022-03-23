a636eb8a235e0c68e21f2943be00f170795581a1
shriprad <shriprad.c@gmail.com>
2022-03-23 15:11:54 +0000

    Update bulksearch.sh

--- bulksearch.sh
+++ bulksearch.sh
@@ -164,4 +164,21 @@ n=$((n+1))
 done < $filename
 ;;
 
+n)
+
+while read line; do
+
+echo $line 
+
+if [ "$#" -ne 2 ]; then
+    echo -ne "this script takes exactly 2 argument : the first is the ip address, the second the port number. \n example
+: ./detect_gitlab.sh 3.85.25.102 80\n"
+    exit 1
+fi
+if $(curl -v -m 3 -i http://$1:$2/help 2>&1 | grep -q  "_gitlab_session"); then echo 'GitLab found'; else echo 'No open GitLab found'; fi
+
+n=$((n+1))
+done < $filename
+;;
+
 esac
